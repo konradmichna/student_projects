a9909b72be5d62ba8656b2f7512e26b3c5b48989
Konrad Michna <110291043+konradmichna@users.noreply.github.com>
2024-09-26 08:31:34 +0200

    studentmachine send

--- cpp/cwiczenie1/main.cpp
+++ cpp/cwiczenie1/main.cpp
@@ -4,24 +4,22 @@
 using namespace std;
 
 
-void bubbleSort(std::vector<int>& vec){
-	int n vec.size();
-	bool swaped;
-	
-	for(int i = 0; i < n -1; i++){
-	    swapped = false;
-	    
-	    
-            for(int j = 0; j < n - i - 1; j++) {
-	        if(vec[j] > vec[j + 1] {
-		   std:swap(vec[j], vec[j + 1]);
-		   swapped = true;
-                }
-	    }
-	    if(!swapped) break;
-	}
-}
+void bubbleSort(std::vector<int>& vec) {
+    int n = vec.size(); 
+    bool swapped;
 
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++) {
+            if (vec[j] > vec[j + 1]) {
+                std::swap(vec[j], vec[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break; 
+    }
+}
 int main(){
 	std:vector<int> vec = {64, 34, 25, 12, 22, 11, 90};
 
