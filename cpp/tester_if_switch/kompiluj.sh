f1f226e838ce1a8e03f79fb0cbdb3690bcd56dd0
Konrad Michna <110291043+konradmichna@users.noreply.github.com>
2024-10-03 07:52:09 +0200

    studentmachine send

--- cpp/tester_if_switch/kompiluj.sh
+++ cpp/tester_if_switch/kompiluj.sh
@@ -1,1 +1,12 @@
+#!/bin/sh
+rm main_test
+clear
+echo "----Kompilacja benchmarka testujacego----"
 
+g++ -std=c++11 tester.cpp -o main_test -lgtest_main -lgtest -lbenchmark -lpthread
+
+echo "---Kompilacja benchmarka ukonczona----"
+echo "---Uruchamianie beanchmarka---"
+chmod +x main_test
+./main_test
+echo "---Zakonczono benchmark---"
