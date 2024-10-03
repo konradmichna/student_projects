ce7de12b90f58af84e8d1d194b89a58a419ff470
Konrad Michna <110291043+konradmichna@users.noreply.github.com>
2024-10-03 07:49:46 +0200

    studentmachine send

--- cpp/tester_if_switch/tester.cpp
+++ cpp/tester_if_switch/tester.cpp
@@ -1,1 +1,74 @@
+#include <iostream>
+#include <chrono>
+#include <gtest/gtest.h>
 
+using namespace std;
+
+// Funkcja używająca instrukcji if-else
+int compareIf(int value) {
+    if (value == 1) {
+        return 10;  
+    }
+    else if (value == 2) {
+        return 20;  
+    }
+    else if (value == 3) {
+        return 30;  
+    }
+    else {
+        return 40;  
+    }
+}
+
+// Funkcja używająca instrukcji switch
+int compareSwitch(int value) {
+    switch (value) {
+        case 1:
+            return 10;  
+        case 2:
+            return 20;  
+        case 3:
+            return 30;  
+        default:
+            return 40;  
+    }
+}
+
+// Benchmark dla instrukcji if-else
+TEST(BenchmarkTest, IfElseBenchmark) {
+    const int iterations = 10000;
+    int sumIf = 0;
+
+    // Pomiar czasu dla if-else
+    auto startIf = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < iterations; i++) {
+        sumIf += compareIf(i % 4);  // Wywołanie compareIf z wartościami 0, 1, 2, 3
+    }
+    auto endIf = std::chrono::high_resolution_clock::now();
+    auto durationIf = std::chrono::duration_cast<std::chrono::microseconds>(endIf - startIf);
+
+    // Wyświetlanie wyników
+    cout << "Czas wykonania instrukcji if-else: " << durationIf.count() << " mikrosekund" << endl;
+}
+
+// Benchmark dla instrukcji switch
+TEST(BenchmarkTest, SwitchBenchmark) {
+    const int iterations = 10000;
+    int sumSwitch = 0;
+
+    // Pomiar czasu dla switch
+    auto startSwitch = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < iterations; i++) {
+        sumSwitch += compareSwitch(i % 4);  // Wywołanie compareSwitch z wartościami 0, 1, 2, 3
+    }
+    auto endSwitch = std::chrono::high_resolution_clock::now();
+    auto durationSwitch = std::chrono::duration_cast<std::chrono::microseconds>(endSwitch - startSwitch);
+
+    // Wyświetlanie wyników
+    cout << "Czas wykonania instrukcji switch: " << durationSwitch.count() << " mikrosekund" << endl;
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
