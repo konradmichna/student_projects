3c4bb572f27df7ee0b9f10d6cb5033548bad0768
Konrad Michna <110291043+konradmichna@users.noreply.github.com>
2024-10-09 08:00:41 +0200

    studentmachine send

--- cpp/unit_tests_fizzbuzz/main_test.cpp
+++ cpp/unit_tests_fizzbuzz/main_test.cpp
@@ -1,25 +1,32 @@
-// main_test.cpp
 #include <gtest/gtest.h>
 #include "functions.cpp"
 
-
-TEST(mojagrupatestow, mojtest) {
-   #TODO: tu napisz test
+// Test dla liczby podzielnej przez 3
+TEST(FizzBuzzTest, MultiplesOf3) {
+    EXPECT_EQ(fizzBuzz(3), "Fizz");
+    EXPECT_EQ(fizzBuzz(9), "Fizz");
 }
 
-TEST(mojagrupatestow, mojtest) {
-   #TODO: tu napisz test
+// Test dla liczby podzielnej przez 5
+TEST(FizzBuzzTest, MultiplesOf5) {
+    EXPECT_EQ(fizzBuzz(5), "Buzz");
+    EXPECT_EQ(fizzBuzz(10), "Buzz");
 }
 
-TEST(mojagrupatestow, mojtest) {
-   #TODO: tu napisz test
+// Test dla liczby podzielnej przez 3 i 5
+TEST(FizzBuzzTest, MultiplesOf3And5) {
+    EXPECT_EQ(fizzBuzz(15), "FizzBuzz");
+    EXPECT_EQ(fizzBuzz(30), "FizzBuzz");
 }
 
+// Test dla liczby, która nie jest podzielna przez 3 ani 5
+TEST(FizzBuzzTest, NeitherMultipleOf3Nor5) {
+    EXPECT_EQ(fizzBuzz(1), "1");
+    EXPECT_EQ(fizzBuzz(7), "7");
+}
 
-// ... inne testy ...
-
+// Inicjalizacja testów
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
