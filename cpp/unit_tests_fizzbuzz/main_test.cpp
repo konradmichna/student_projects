74e5ae4b4c595824103ed0b099f904e5aaeb56ab
Konrad Michna <110291043+konradmichna@users.noreply.github.com>
2024-10-09 08:01:17 +0200

    studentmachine send

--- cpp/unit_tests_fizzbuzz/main_test.cpp
+++ cpp/unit_tests_fizzbuzz/main_test.cpp
@@ -3,26 +3,26 @@
 
 // Test dla liczby podzielnej przez 3
 TEST(FizzBuzzTest, MultiplesOf3) {
-    EXPECT_EQ(fizzBuzz(3), "Fizz");
-    EXPECT_EQ(fizzBuzz(9), "Fizz");
+    ASSERT_EQ(fizzBuzz(3), "Fizz");
+    ASSERT_EQ(fizzBuzz(9), "Fizz");
 }
 
 // Test dla liczby podzielnej przez 5
 TEST(FizzBuzzTest, MultiplesOf5) {
-    EXPECT_EQ(fizzBuzz(5), "Buzz");
-    EXPECT_EQ(fizzBuzz(10), "Buzz");
+    ASSERT_EQ(fizzBuzz(5), "Buzz");
+    ASSERT_EQ(fizzBuzz(10), "Buzz");
 }
 
 // Test dla liczby podzielnej przez 3 i 5
 TEST(FizzBuzzTest, MultiplesOf3And5) {
-    EXPECT_EQ(fizzBuzz(15), "FizzBuzz");
-    EXPECT_EQ(fizzBuzz(30), "FizzBuzz");
+    ASSERT_EQ(fizzBuzz(15), "FizzBuzz");
+    ASSERT_EQ(fizzBuzz(30), "FizzBuzz");
 }
 
 // Test dla liczby, która nie jest podzielna przez 3 ani 5
 TEST(FizzBuzzTest, NeitherMultipleOf3Nor5) {
-    EXPECT_EQ(fizzBuzz(1), "1");
-    EXPECT_EQ(fizzBuzz(7), "7");
+    ASSERT_EQ(fizzBuzz(1), "1");
+    ASSERT_EQ(fizzBuzz(7), "7");
 }
 
 // Inicjalizacja testów
